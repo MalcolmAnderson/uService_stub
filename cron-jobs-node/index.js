5ce5952db27b8dafe8c7684cf4ce237b5bf5f929
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-05-28 16:42:40 -0700

    Ugly WIP - does work, after a fashion

--- cron-jobs-node/index.js
+++ cron-jobs-node/index.js
@@ -1,26 +1,87 @@
 //index.js
-const cron = require('node-cron');
 const schedule = require('node-schedule');
 const express = require('express');
 const fs = require('fs');
+const bunyan = require('bunyan');
+let config = require('config');
 
-let configFileName = "./config.config";
-let configCheckInSeconds = 2;
+let configFileName = "./config/default.json";
+let configCheckInSeconds = 1;
+let displayFileCheckingDialog = false;
+let infoLogFrequencyInSeconds = 5;
+let errorLogFrequencyInSeconds = 5;
+let quickconfigCheckFrequencyInSeconds = 10;
+
 let configLastModifiedTime = getFileLastModifiedTimeInMilliseconds(configFileName);
 
-// console.log(`configLastModifiedTime:                  "${configLastModifiedTime}"`);
-// console.log(`getFileLastModifiedTime(configFileName): "${getFileLastModifiedTimeInMilliseconds(configFileName)}"`);
+let logConfiguration = config.get("Bunyan")
+const log = bunyan.createLogger(logConfiguration);
+let flipper = true;
+//let log = bunyan.createLogger({ name: "myapp", level: "Error" });
+//let bunyanLevel = config.get("Bunyan.level");
+//console.log(bunyanLevel);
+//log.level("INFO");
+
+//const log = bunyan.createLogger({ name: "myapp", level: "Info" });
+
 let valuesAreEqual = configLastModifiedTime == getFileLastModifiedTimeInMilliseconds(configFileName)
 
-// console.log('valuesAreEqual == ' + valuesAreEqual);
 
-var j = schedule.scheduleJob({ rule: `*/${configCheckInSeconds} * * * * *` }, function () {
-    if(configLastModifiedTime == getFileLastModifiedTimeInMilliseconds(configFileName)){
-        console.log('no change to config file.');
+// var j = schedule.scheduleJob({ rule: `*/${configCheckInSeconds} * * * * *` }, function () {
+//     if (configLastModifiedTime == getFileLastModifiedTimeInMilliseconds(configFileName)) {
+//         if (displayFileCheckingDialog) {
+//             console.log('no change to config file.');
+//         }
+//     } else {
+//         if (displayFileCheckingDialog) {
+//             console.log('Time for do the thing, and update the last modified time stamp!');
+//         }
+//         configLastModifiedTime = getFileLastModifiedTimeInMilliseconds(configFileName);
+//         //log = bunyan.createLogger({ name: "myapp", level: "Info" });
+
+//         //console.log(`logConfiguration: ${bunyanLevel}`);
+//         //console.log(logConfiguration);
+
+//         global.NODE_CONFIG = null;
+//         delete require.cache.config;
+//         var config = require('config');
+
+//         delete require.cache[require.resolve('config')];
+//         // config = require('config');
+//         // logConfiguration = config.get("Bunyan")
+//         // console.log(logConfiguration);
+//         let bunyanLevel = config.get("Bunyan.level");
+//         log.level = bunyanLevel;
+
+
+//         //log = bunyan.createLogger(logConfiguration);
+
+//         console.log(`logConfiguration: ${bunyanLevel}`);
+//         //console.log(logConfiguration);
+//         //log.level = bunyanLevel;
+//     }
+// });
+
+var k = schedule.scheduleJob({ rule: `*/${infoLogFrequencyInSeconds} * * * * *` }, function () {
+    log.info({ infoPayload: 'INFO log entry' });
+});
+
+var m = schedule.scheduleJob({ rule: `*/${errorLogFrequencyInSeconds} * * * * *` }, function () {
+    log.error({ errorPayload: 'ERROR log entry' });
+});
+
+var n = schedule.scheduleJob({ rule: `*/${quickconfigCheckFrequencyInSeconds} * * * * *` }, function () {
+    delete require.cache[require.resolve('log')];
+    log = bunyan.createLogger(logConfiguration);
+    //console.log(config.get("Bunyan.level"));
+    if (flipper) {
+        log.level = "INFO";
     } else {
-        console.log('Time for do the thing, and update the last modifide time stamp!');
-        configLastModifiedTime = getFileLastModifiedTimeInMilliseconds(configFileName);
+        log.level = "ERROR";
     }
+    flipper = !flipper;
+    console.log(flipper);
+    console.log(log.level);
 });
 
 app = express();
